f4fadb9b9b57fc650e9fdf03beee2a9a068be810
joaoveasey <jovepedro@gmail.com>
2024-10-16 19:54:33 -0300

    feat: configurando o DbContext da aplicação

--- login.api/Program.cs
+++ login.api/Program.cs
@@ -1,10 +1,19 @@
 using FluentValidation;
+using login.api.Infra;
 using login.api.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var serverVersion = new MySqlServerVersion(new Version(8, 0, 21));
+    options.UseMySql(connectionString, serverVersion);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
